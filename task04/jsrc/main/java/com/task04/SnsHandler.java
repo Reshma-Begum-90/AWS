de3113fcb6b1da58e7fedb12919353733f1c2afd
Reshma <reshma_begum@epam.com>
2025-03-12 11:36:41 +0530

    commit

--- task04/jsrc/main/java/com/task04/SnsHandler.java
+++ task04/jsrc/main/java/com/task04/SnsHandler.java
@@ -3,18 +3,19 @@ package com.task04;
 import com.amazonaws.services.lambda.runtime.Context;
 import com.amazonaws.services.lambda.runtime.RequestHandler;
 import com.syndicate.deployment.annotations.lambda.LambdaHandler;
-import com.syndicate.deployment.annotations.events.SnsEventSource;
 import com.syndicate.deployment.model.RetentionSetting;
+import com.syndicate.deployment.annotations.events.SnsEventSource;
+
 
 import java.util.HashMap;
 import java.util.Map;
 
 @LambdaHandler(
-		lambdaName = "sns_handler",
-		roleName = "sns_handler-role",
-		isPublishVersion = true,
-		aliasName = "learn",
-		logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
+    lambdaName = "sns_handler",
+	roleName = "sns_handler-role",
+	isPublishVersion = true,
+	aliasName = "${lambdas_alias_name}",
+	logsExpiration = RetentionSetting.SYNDICATE_ALIASES_SPECIFIED
 )
 @SnsEventSource(
 		targetTopic = "lambda_topic"
